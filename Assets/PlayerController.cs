dc901df4a1f0a8b3226341a0db16088e3baee6f9
aslampangestu <aslampangestu@gmail.com>
2018-12-01 23:51:41 +0700

    init

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour {
+
+	public float playerSpeed = 600f;
+	float movement = 0f;
+	public GameObject gameOver;
+	// Use this for initialization
+	void Start () {
+		gameOver.SetActive(false);		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		movement = Input.GetAxisRaw("Horizontal");
+	}
+
+	private void FixedUpdate() {
+		transform.RotateAround(Vector3.zero, Vector3.forward, movement * Time.fixedDeltaTime * -playerSpeed);
+	}
+
+	private void OnTriggerEnter2D(Collider2D other) {
+		gameOver.SetActive(true);
+        Time.timeScale = 0f;
+	}
+
+	public void TryAgain(){
+		gameOver.SetActive(false);
+        Time.timeScale = 1f;
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+}
---
+++ Assets/Score.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+	public float score;
+	public Text scoreTxt,scoreFinalTxt;
+	// Use this for initialization
+	void Start () {
+		score = 0f;
+		scoreTxt.text = score.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		score += Time.deltaTime;
+		scoreTxt.text = Mathf.RoundToInt(score).ToString();
+		scoreFinalTxt.text = Mathf.RoundToInt(score).ToString();
+	}
+}
